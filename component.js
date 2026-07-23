7d80e74817f156b8b470b20a75fb44f5ceae9d04
clarkk <clark@electrobeat.dk>
2026-07-23 01:17:03 +0000

    update

---
+++ component.js
@@ -0,0 +1,9 @@
+export function loader(size='3rem', invert=false){
+	return `
+		<div class="loader ${invert ? 'invert' : ''}" style="--loader-size:${size}">
+			<svg viewBox="25 25 50 50">
+				<circle cx="50" cy="50" r="20"></circle>
+			</svg>
+		</div>
+	`;
+}
