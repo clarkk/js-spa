07a0e7bcb5321a74d1513689b6415edd5cff3111
clarkk <clark@electrobeat.dk>
2026-07-25 11:33:34 +0000

    update

--- component.js
+++ component.js
@@ -7,3 +7,19 @@ export function loader(size='3rem', invert=false){
 		</div>
 	`;
 }
+
+export function http_error(err){
+	const messages = {
+		400: 'Bad request',
+		401: 'Unauthorized',
+		403: 'Forbidden',
+		404: 'Not found',
+		500: 'Internal server error'
+	}, status = err.status ?? err, text = err.body?.error?.request || messages[status] || 'Error';
+	return `
+		<div class="text-center">
+			<h1><i class="bi bi-x-circle"></i> HTTP ${status}</h1>
+			<p>${text}</p>
+		</div>
+	`;
+}
