bb5d6f870942abfbba8a85374eced9a6d2acc634
clarkk <clark@electrobeat.dk>
2026-08-04 14:56:25 +0000

    update

--- form_dropdown.js
+++ form_dropdown.js
@@ -4,6 +4,8 @@ import * as frm from 'frm';
 
 let active = null, position_frame = null, panel = null;
 
+const CLASS_SELECTED='selected';
+
 export function create(store, parent, value){
 	let container = null, input = null, input_select = null, rendered = false, opened = false, component = null, definition = null;
 	
@@ -73,16 +75,30 @@ export function create(store, parent, value){
 				return true;
 				
 			case frm.KEY_ARROW_DOWN:
+				e.preventDefault();
+				open();
+				component.down();
+				return true;
+				
 			case frm.KEY_ARROW_UP:
 				e.preventDefault();
 				open();
+				component.up();
 				return true;
 				
 			case frm.KEY_ENTER:
+				if(!opened) return false;
+				
+				e.preventDefault();
+				component.choose();
+				close();
+				return true;
+				
 			case frm.KEY_TAB:
 				if(!opened) return false;
 				
 				e.preventDefault();
+				component.choose();
 				close();
 				parent.tab(e);
 				return true;
@@ -143,7 +159,7 @@ export function create(store, parent, value){
 }
 
 function create_list(store, def, input, input_select){
-	let options = [], filtered = [], selected = -1;
+	let options = [], filtered = [], selected = -1, items = null;
 	const o = {
 		init(value){
 			options = def.options.map(([value, text])=>({
@@ -166,16 +182,50 @@ function create_list(store, def, input, input_select){
 				<div class="field-dropdown-label">${store.t(def.label)}</div>
 				<ul class="field-dropdown-list">
 					${filtered.map((option, i)=>`
-						<li data-index="${i}">${fmt.html(option.text)}</li>
+						<li class="${i === selected ? CLASS_SELECTED : ''}" data-index="${i}">${fmt.html(option.text)}</li>
 					`).join('')}
 				</ul>
 			`;
+			items = panel.querySelectorAll('li');
 			return o;
+		},
+		down(){
+			move(1);
+		},
+		up(){
+			move(-1);
+		},
+		choose(){
+			if(selected === -1) return;
+			
+			const option = filtered[selected];
+			if(o.select()){
+				input_select.value = option.value;
+				input.value = option.text;
+			}
+			else{
+				
+			}
 		},
 		select(){
 			return !!def.select;
 		}
 	};
+	
+	function move(direction){
+		if(!filtered.length) return;
+		
+		const previous = selected;
+		selected += direction;
+		if(selected < 0) selected = filtered.length - 1;
+		else if(selected >= filtered.length) selected = 0;
+		
+		items[previous]?.classList.remove(CLASS_SELECTED);
+		items[selected]?.classList.add(CLASS_SELECTED);
+		items[selected]?.scrollIntoView({
+			block: 'nearest'
+		});
+	}
 	
 	function filter(){
 		if(o.select()) filtered = options;
