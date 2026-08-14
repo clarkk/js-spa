6da3a2687277f2765c0c3545083ec5579d1f8b39
clarkk <clark@electrobeat.dk>
2026-08-14 18:31:54 +0000

    udpate

--- dom.js
+++ dom.js
@@ -4,6 +4,26 @@ export function id(){
 	return 'e'+dom_id_prefix+dom_id++;
 }
 
+export function html_before(elm, html, mounted=null){
+	elm.insertAdjacentHTML('beforebegin', html);
+	if(mounted) for(const fn of mounted) fn();
+}
+
+export function html_after(elm, html, mounted=null){
+	elm.insertAdjacentHTML('afterend', html);
+	if(mounted) for(const fn of mounted) fn();
+}
+
+export function html_prepend(elm, html, mounted=null){
+	elm.insertAdjacentHTML('afterbegin', html);
+	if(mounted) for(const fn of mounted) fn();
+}
+
+export function html_append(elm, html, mounted=null){
+	elm.insertAdjacentHTML('beforeend', html);
+	if(mounted) for(const fn of mounted) fn();
+}
+
 /*export function scrollbar_width(elm){
 	return elm.offsetWidth - elm.clientWidth;
 }
